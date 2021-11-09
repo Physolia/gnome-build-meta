2fc7dad161b8a348f2b17d3f2f8f1f904014dbf0
Sam Thursfield <sam.thursfield@codethink.co.uk>
2021-11-09 11:40:49 +0100

    openqa: Move tests into separate repository
    See https://gitlab.gnome.org/GNOME/gnome-build-meta/-/issues/453 for
    rationale.

--- openqa/main.pm
+++
@@ -1,9 +0,0 @@
-use strict;
-use testapi;
-use autotest;
-use needle;
-
-autotest::loadtest "tests/gnome_install.pm";
-autotest::loadtest "tests/gnome_welcome.pm";
-autotest::loadtest "tests/gnome_desktop.pm";
-1;
